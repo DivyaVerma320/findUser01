d59db13ae360229e88601a52bc388d2c92df5948
DivyaVerma320 <divyaver1967@gmail.com>
2022-06-14 14:17:34 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import UserFinder from './components/UserFinder';
+import UsersContext from './store/user_Context';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
+
+function App() {
+  const usersContext = {
+    users : DUMMY_USERS
+  }
+  return (
+    <div>
+      <UsersContext.Provider value={usersContext}>
+      <UserFinder />
+      </UsersContext.Provider>
+    </div>
+  );
+}
+
+export default App;
